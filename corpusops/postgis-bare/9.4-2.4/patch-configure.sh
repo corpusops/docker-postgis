10c822faa49a10d22954585ad92e1404f432bb9e
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-01-03 22:25:32 +0000

    grep->grep

--- corpusops/postgis-bare/9.4-2.4/patch-configure.sh
+++ corpusops/postgis-bare/9.4-2.4/patch-configure.sh
@@ -2,7 +2,7 @@
 set -ex
 for i in $@;do
 if ( grep -q ACCEPT_USE_OF_DEPRECATED_PROJ_API_H $i);then continue;fi
-if ( egrep -q '^AC_CHECK_HEADER\(\[proj_api.h\],$' $i );then continue;fi
+if ( grep -E -q '^AC_CHECK_HEADER\(\[proj_api.h\],$' $i );then continue;fi
 
 sed -i -r \
     -e "s/AC_CHECK_HEADER..proj_api.h.*/AC_CHECK_HEADER([proj_api.h], [], [AC_MSG_ERROR([could not find proj_api.h - you may need to specify the directory of a PROJ.4 installation using --with-projdir])],[\\n#define ACCEPT_USE_OF_DEPRECATED_PROJ_API_H 1\\n])\\n\\n/g" $i
