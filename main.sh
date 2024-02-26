01dfed81a24b4e441ad4e0791f02b13113aae9ca
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2024-02-26 19:22:49 +0200

    sync

--- main.sh
+++ main.sh
@@ -253,11 +253,10 @@ SKIP_TF="(tensorflow.serving:[0-9].*)"
 SKIP_MINIO="(k8s-operator|((minio|mc):(RELEASE.)?[0-9]{4}-.{7}))"
 SKIP_MAILU="(mailu.*(feat|patch|merg|refactor|revert|upgrade|fix-|pr-template))"
 SKIP_DOCKER="docker(\/|:)([0-9]+\.[0-9]+\.|17|18.0[1-6]|1$|1(\.|-)).*"
-SKIPPED_TAGS="$SKIP_TF|$SKIP_MINOR_OS|$SKIP_NODE|$SKIP_DOCKER|$SKIP_MINIO|$SKIP_MAILU|$SKIP_MINOR_ES2|$SKIP_MINOR|$SKIP_PRE|$SKIP_OS|$SKIP_PHP|$SKIP_WINDOWS|$SKIP_MISC"
 CURRENT_TS=$(date +%s)
 IMAGES_SKIP_NS="((mailhog|postgis|pgrouting(-bare)?|^library|dejavu|(minio/(minio|mc))))"
 
-SKIP_POSTGIS="$SKIP_MISC|$SKIP_PRE|post.*:.*1[3-9].-[0-3]|postgres:(.*beta.*|.*alpine3.*|.*alpine.*|9\.[0-9]+\.[0-9]+.*|9\.0|8.*|1[09]\.[0-9].*)$|postgis.*:(9|10|11)\.|:.*alpine.*"
+SKIP_POSTGIS="$SKIP_MISC|$SKIP_PRE|postgis.*:(.*1[3-9].-[0-3]|9|10|11)\.|.*alpine.*)"
 SKIPPED_TAGS="$SKIP_POSTGIS"
 default_images="
 corpusops/postgis-bare
